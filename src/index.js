ae031bbf9ac3bdf82fa65836241985a4f209aed2
YA <vector_cp@mail.ru>
2022-05-07 12:49:04 +0300

    feat: keyboard => Shift: add funtional

--- src/index.js
+++ src/index.js
@@ -10,25 +10,24 @@ import { keyLayout } from './data/keysData';
 import { keysLayoutComponent } from './components/keysLayoutComponent';
 
 import { appendElemToDOM } from './helpers/appendElemToDOM';
-import { replaceElemToDOM } from './helpers/replaceElemToDOM';
+import { delElemFromDOM } from './helpers/delElemFromDOM';
 
 // INIT
+
+let layout = { lang: 'en', number: 0 };
+
 appendElemToDOM(document.body, app);
 appendElemToDOM(app, [textarea, keyboard, footer]);
 
-let keysContainer = keysLayoutComponent(keyLayout, 0);
+let keysContainer = keysLayoutComponent(
+  keyLayout,
+  layout.number
+);
 
 appendElemToDOM(keyboard, keysContainer);
 
-// replaceElemToDOM(
-//   keyboard,
-//   keysContainer,
-//   keysLayoutComponent(keyLayout, 3)
-// );
-
 // HANDLERS
 const keypressHandler = (e) => {
-  console.log('keypressHandler');
   if (e.keyCode) {
     textarea.focus();
     if (textarea.value === '') {
@@ -38,52 +37,171 @@ const keypressHandler = (e) => {
       `[data-name="${e.key}"]`
     );
     el.classList.add('virtual__key--pressed');
-    //  console.log(el);
     setTimeout(() => {
       el.classList.remove('virtual__key--pressed');
     }, 400);
   }
 };
+
 const keydownHandler = (e) => {
-  // console.log(e);
+  const currentKey = e.code;
+
+  console.log(currentKey);
+
+  // console.log('->',e.code);
+  // console.log('-->', e.keyCode);
+  // console.log('-->', e.key);
+  // console.log('-->', e.charCode);
+  switch (currentKey) {
+    case 'ShiftLeft':
+      if (layout.number === 1) {
+        break;
+      }
+      if (layout.number === 0) {
+        layout.number = 1;
+      }
+      delElemFromDOM(keysContainer);
+      keysContainer = keysLayoutComponent(
+        keyLayout,
+        layout.number
+      );
+      appendElemToDOM(keyboard, keysContainer);
+      break;
+
+    case 'ShiftRight':
+      if (layout.number === 1) {
+        break;
+      }
+      if (layout.number === 0) {
+        layout.number = 1;
+      }
+      delElemFromDOM(keysContainer);
+      keysContainer = keysLayoutComponent(
+        keyLayout,
+        layout.number
+      );
+      appendElemToDOM(keyboard, keysContainer);
+      break;
+
+    default:
+      break;
+  }
 };
 const keyupHandler = (e) => {
-  // console.log(e);
+  const currentKey = e.code;
+  switch (currentKey) {
+    case 'ShiftLeft':
+      if (layout.number === 1) {
+        layout.number = 0;
+      }
+      delElemFromDOM(keysContainer);
+      keysContainer = keysLayoutComponent(
+        keyLayout,
+        layout.number
+      );
+      appendElemToDOM(keyboard, keysContainer);
+      break;
+
+    case 'ShiftRight':
+      layout.number = layout.lang = 'en' ? 0 : 2;
+      delElemFromDOM(keysContainer);
+      keysContainer = keysLayoutComponent(
+        keyLayout,
+        layout.number
+      );
+      appendElemToDOM(keyboard, keysContainer);
+      break;
+
+    default:
+      break;
+  }
 };
 
 const clickHandler = (e) => {
-  console.log('clickHandler');
-  // console.log(textarea.selectionStart);
+  const currentKey = e.target.dataset.name;
   if (e.target.tagName !== 'BUTTON') {
     textarea.focus();
     return;
   }
 
-//anim
+  if (currentKey.length > 1) {
+    switch (
+      currentKey //special
+    ) {
+      case ' Shift ':
+        if (layout.number === 0) {
+          layout.number = 1;
+        } else if (layout.number === 1) {
+          layout.number = 0;
+        }
+        delElemFromDOM(keysContainer);
+        keysContainer = keysLayoutComponent(
+          keyLayout,
+          layout.number
+        );
+        appendElemToDOM(keyboard, keysContainer);
+        const el = document.querySelector(
+          `[data-name=" Shift "]`
+        );
+        if (layout.number == 1 || layout.number == 3) {
+          el.classList.add(
+            'virtual__key--permanent-pressed'
+          );
+        }
+        break;
 
-  textarea.focus();
-  const currentValue = textarea.value;
-  const currentPosition = textarea.selectionStart;
+      case 'Shift':
+        if (layout.number === 0) {
+          layout.number = 1;
+        } else if (layout.number === 1) {
+          layout.number = 0;
+        }
+        delElemFromDOM(keysContainer);
+        keysContainer = keysLayoutComponent(
+          keyLayout,
+          layout.number
+        );
+        appendElemToDOM(keyboard, keysContainer);
+        const el1 = document.querySelector(
+          `[data-name="Shift"]`
+        );
+        if (layout.number == 1 || layout.number == 3) {
+          el1.classList.add(
+            'virtual__key--permanent-pressed'
+          );
+        }
+        break;
 
-  const currentValueLeft = currentValue.slice(
-    0,
-    currentPosition
-  );
-  const currentValueRight = currentValue.slice(
-    currentPosition,
-    currentValue.length
-  );
-  // console.log(`${currentValueLeft}---${currentValueRight}`);
-  textarea.value =
-    currentValueLeft +
-    e.target.dataset.name +
-    currentValueRight;
-  textarea.setSelectionRange(
-    currentPosition + 1,
-    currentPosition + 1
-  );
-  // console.log(textarea.selectionStart);
+      default:
+        break;
+    }
+  } else {
+    // keys
+    // textarea
+    textarea.focus();
+    const currentValue = textarea.value;
+    const currentPosition = textarea.selectionStart;
+
+    const currentValueLeft = currentValue.slice(
+      0,
+      currentPosition
+    );
+    const currentValueRight = currentValue.slice(
+      currentPosition,
+      currentValue.length
+    );
+    textarea.value =
+      currentValueLeft + currentKey + currentValueRight;
+    textarea.setSelectionRange(
+      currentPosition + 1,
+      currentPosition + 1
+    );
+  }
 };
+
+const mousedownHandler = (e) => {};
+
+const mouseupHandler = (e) => {};
 
 // EVENTS
 document.body.addEventListener('keypress', keypressHandler);
@@ -91,4 +209,6 @@ document.body.addEventListener('keypress', keypressHandler);
 document.body.addEventListener('keydown', keydownHandler);
 document.body.addEventListener('keyup', keyupHandler);
 
-keysContainer.addEventListener('click', clickHandler);
+keyboard.addEventListener('click', clickHandler);
+keyboard.addEventListener('mousedown', mousedownHandler);
+keyboard.addEventListener('mouseup', mouseupHandler);
