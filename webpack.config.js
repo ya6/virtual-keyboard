538926f2fc8156befc2eb34000417fbdf411b413
YA <vector_cp@mail.ru>
2022-05-08 19:57:02 +0300

    feat: webpack.config: change clean ignore dir, HtmlWebpackPlugin: make simple

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,8 @@ const config = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: {
-      keep: /assets/, // Keep these assets under 'ignored/dir'.
+      // keep: /assets/, // Keep these assets under 'ignored/dir'.
+      keep: /\.git/,
     },
   },
   devServer: {
@@ -29,17 +30,18 @@ const config = {
     new HtmlWebpackPlugin({
       // template: 'index.html',
       favicon: './src/assets/favicon.ico',
+      title: 'virtual-keyboard'
 
-      templateContent: `
-        <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-        rel="stylesheet">
-        <title>virtual-keyboard</title>
-        </head>
-        `,
+      // templateContent: `
+      //   <head>
+      //   <meta charset="UTF-8">
+      //   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      //   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      //   <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      //   rel="stylesheet">
+      //   <title>virtual-keyboard</title>
+      //   </head>
+      //   `,
     }),
 
     // Add your plugins here
